a60df02ed3a03be0d74db95701d6901cd62f7116
Mikhail Korotkikh <mishakor23@gmail.com>
2016-09-26 17:20:37 +0300

    add functional to create emploees

--- src/app/emploees/emploees-add-form/emploees-add-form.component.ts
+++ src/app/emploees/emploees-add-form/emploees-add-form.component.ts
@@ -1,14 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EmploeesService } from '../emploees.service';
+import { Emploee } from '../emploee';
+
 @Component({
   selector: 'app-emploees-add-form',
   templateUrl: './emploees-add-form.component.html'
 })
 export class EmploeesAddFormComponent implements OnInit {
+  // emploee: Emploee;
+  newEmploee = {
+    firstName: '',
+    lastName: '',
+    role: '',
+    technicalExpertise: '',
+    skills: '',
+    communication: '',
+    leadership: '',
+    education: ''
+  }
 
-  constructor() { }
+  constructor(private emploeesService: EmploeesService) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    const { firstName,
+            lastName,
+            role,
+            technicalExpertise,
+            skills,
+            communication,
+            leadership,
+            education
+            } = this.newEmploee;
+    console.log(this.newEmploee);
+
+    this.emploeesService.createEmploee(this.newEmploee);
+    this.newEmploee = {
+      firstName: '',
+      lastName: '',
+      role: '',
+      technicalExpertise: '',
+      skills: '',
+      communication: '',
+      leadership: '',
+      education: ''
+    }
+  }
 }
--- src/app/emploees/emploees.service.ts
+++ src/app/emploees/emploees.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Injectable()
 export class EmploeesService {
+  emploees: FirebaseListObservable<any>;
+  constructor(private af: AngularFire) {
+    this.emploees =  this.af.database.list('emploees');
+  }
 
-  constructor() { }
+  getEmploees() {
+    return this.emploees;
+  }
 
+  createEmploee(newEmploee) {
+    this.emploees.push(newEmploee);
+  }
 }
