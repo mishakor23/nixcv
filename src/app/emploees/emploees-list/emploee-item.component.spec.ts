7c14db8acccaa5c4dc37dcfc52242bc34210db10
Mikhail Korotkikh <mishakor23@gmail.com>
2016-09-26 15:11:14 +0300

    add emploee-item component

---
+++ src/app/emploees/emploees-list/emploee-item.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { EmploeeItemComponent } from './emploee-item.component';
+
+describe('Component: EmploeeItem', () => {
+  it('should create an instance', () => {
+    let component = new EmploeeItemComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/emploees/emploees-list/emploee-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emploee-item',
+  templateUrl: './emploee-item.component.html',
+  styleUrls: ['./emploee-item.component.scss']
+})
+export class EmploeeItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
