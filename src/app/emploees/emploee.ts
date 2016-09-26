c60ed70078a10d56b9b1c3379af80c44a313ae7f
Mikhail Korotkikh <mishakor23@gmail.com>
2016-09-26 13:34:33 +0300

    create emploee class

---
+++ src/app/emploees/emploee.ts
@@ -0,0 +1,12 @@
+export class Emploee {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public role: string,
+    public technicalExpertise: string,
+    public skills: string,
+    public communication: string,
+    public leadership: string,
+    public education: string
+  ) {}
+}
